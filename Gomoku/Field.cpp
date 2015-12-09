52b0fe431736bdc11d320e7a5f2f6834cbdd7b4b
Gary <Garyshemyakin@gmail.com>
2015-12-09 07:35:46 +0300

    nightly build 09.12.15 v2

--- Gomoku/Field.cpp
+++ Gomoku/Field.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Field.h"
 
+const char Field::GameSymbols[3] = { ' ', 'X', 'O' };
 
 Field::Field()
 {
@@ -8,13 +9,29 @@ Field::Field()
 //		for (int j = 0; j < size; j++)
 //			_field[i][j] = ' ';
 	for (char c : _field)
-		c = ' ';
+		c = GameSymbols[0];
 	_field[size*size + 1] = 0;
 }
 
-
-Field::~Field()
+/**
+Возвращаемые значения:
+Ошибка: Эксепшн-строка с текстом ошибки
+-1	- игра окончилась ничьей
+0	- игра не окончена
+1	- победил 1-й игрок
+2	- победил 2-й игрок
+*/
+int Field::TakeTurn(int n, int player)
 {
+	int winner;
+	if ((winner = WhoWon(_lastTurn)) != 0)
+		return winner;
+	return 0;
+}
+
+int Field::TakeTurn(int row, int column, int player)
+{
+	return TakeTurn(row*size + column, player);
 }
 
 std::string Field::toString()
@@ -26,21 +43,6 @@ std::string Field::toString()
 	for (char c : _field)
 		result += c;
 	return result;
-}
-
-bool Field::MakeTurn(int n, char symbol)
-{
-	if (_field[n] == ' ')
-	{
-		_field[n] = symbol;
-		return true;
-	}
-	else return false;
-}
-
-bool Field::MakeTurn(int row, int column, char symbol)
-{
-	return MakeTurn(row*size + column, symbol);
 }
 
 //проверяет, находится ли выбранная клетка в пределах поля
@@ -63,27 +65,30 @@ bool Field::IsWithinBounds(int row, int column)
 	1	- победил 1-й игрок
 	2	- победил 2-й игрок
 */
-int Field::WhoWon(int turnIndex)
+int Field::WhoWon(int turnIndex = -1)
 {
+	//такое может быть только на первый ход
+	//в этом случае мы знаем, что игра по-любому продолжается
+	if (turnIndex < 0)
+		return 0;
 	int directions[4][2] = { { 1, 0 }, { 0, 1 }, { -1, 1 }, { 1, 1 } };
+	if (_field[turnIndex])
 	//проверяем каждое направление
 	for (int k = 0; k < 4; k++)
 	{
-		int limit_i = directions[k][0] * 4;
-		int limit_j = directions[k][1] * 4;
 		int count = 0;
 
-		//идём по нему от 4 с меньшей стороны до 4 в большую сторону
-		int i = directions[k][0] * (-4);
-		int j = directions[k][1] * (-4);
-		while (i <= limit_i && j <= limit_j)
+		//идём по нему от от меньшего к большему
+		//всего 9 шагов - по 4 в меньшую и большую стороны (чтобы с текущей клеткой было 5 - это предельные значения)
+		int row = directions[k][0] * (-4);
+		int column = directions[k][1] * (-4);
+		for (int step = 0; step < 9; step++)
 		{
-			//если клетка за пределами
 			//если эта клетка за пределами с "меньшей" стороны, то потом мы можем прийти на поле,
 			//поэтому лучше просто пропустить итерацию цикла, а не выходить из него
-			if (IsWithinBounds(i, j) == false)
+			if (IsWithinBounds(row, column) == false)
 				continue;
-			else if (_field[i*size + j] == _field[turnIndex])
+			else if (_field[row*size + column] == _field[turnIndex])
 			{ //иначе, если мы встретили нужный символ, то увеличиваем счётчик серии
 				count++;
 			}
@@ -94,14 +99,14 @@ int Field::WhoWon(int turnIndex)
 				count = 0;
 			}
 			//двигаемся в нужном нам направлении
-			i += directions[k][0];
-			j += directions[k][1];
+			row += directions[k][0];
+			column += directions[k][1];
 		}
 		if (count >= 5)
 		{
-			if (_field[turnIndex] == playerOneSymbol)
+			if (_field[turnIndex] == 1)
 				return 1; //победил игрок один - крестики
-			if (_field[turnIndex] == playerTwoSymbol)
+			if (_field[turnIndex] == 2)
 				return 2; //победил игрок два - нолики
 		}
 	}
@@ -113,4 +118,22 @@ int Field::WhoWon(int turnIndex)
 int Field::WhoWon(int turnRow, int turnColumn)
 {
 	return WhoWon(turnRow*size + turnColumn);
+}
+
+bool Field::PlaceSymbol(int n, int player)
+{
+	if (_field[n] == ' ')
+	{
+		if (player == 1)
+			_field[n] = 1;
+		if (player == 2)
+			_field[n] = 2;
+		return true;
+	}
+	else return false;
+}
+
+bool Field::PlaceSymbol(int row, int column, int player)
+{
+	return PlaceSymbol(row*size + column, player);
 }
--- Gomoku/Field.h
+++ Gomoku/Field.h
@@ -8,32 +8,48 @@
 class Field
 {
 public:
-	//номер игрока на этой машине
-	int GetPlayerNumber;
 	//размер поля
 	static const int size = SIZE;
+	static const int streak = 5;
+
 	//инициализация поля
 	Field();
-	~Field();
-	//значки игроков
-	const char playerOneSymbol = 'X';
-	const char playerTwoSymbol = 'O';
+	~Field() {};
+
+	//значки игроков и значок незанятого поля
+	//можно играть в "слепое" гомоку - поставить везде пробельчики и играть по памяти
+	static const char GameSymbols[3]; //= { ' ', 'X', 'O' };
+//	const char playerOneSymbol = 'X';
+//	const char playerTwoSymbol = 'O';
+//	const char neutralSymbol = ' ';
+
+	//функция инициации хода
+	int TakeTurn (int n, int player);
+	int TakeTurn (int row, int column, int player);
+
 	//преобразование массива к std::string. Мало ли.
 	std::string toString();
-	//пытаемся сделать ход
-	//если ход валиден, метод вернёт true
-	//если такой ход сделать нельзя (за пределами поля или уже есть знак) - вернёт false
-	bool MakeTurn(int n, char symbol);
-	bool MakeTurn(int row, int column, char symbol);
-	//проверяет по координатам, находится ли клетка внутри поля
-	bool IsWithinBounds(int n);
-	bool IsWithinBounds(int row, int column);
+
 	//определяет, закончена ли игра и кто победил
 	int WhoWon(int turnIndex);
 	int WhoWon(int turnRow, int turnColumn);
 private:
-	int _playerNumber;
+	//проверяет по координатам, находится ли клетка внутри поля
+	bool IsWithinBounds(int n);
+	bool IsWithinBounds(int row, int column);
+
+	//пытаемся поставить знак указанного игрока
+	//если ход валиден, метод вернёт true
+	//если такой ход сделать нельзя (за пределами поля или уже есть знак) - вернёт false
+	bool PlaceSymbol(int n, int player);
+	bool PlaceSymbol(int row, int column, int player);
+
+	int _lastTurn = -1;
+	//номер игрока, который ходит сейчас
+	int _activePlayerNumber = 1;
+	//направления - вниз, вправо, вправо-вниз, вправо-вверх
 	enum Directions { DOWN, RIGHT, DOWNRIGHT, UPRIGHT};
+	//внутреннее представление поля
 	char _field[size*size];
 };
 
